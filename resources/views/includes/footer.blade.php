00174fbd2ea3556abef3f8122c61707d5dc4b87a
Mohit Kumar <mohitkvk455@gmail.com>
2023-11-08 21:28:39 +0530

    first commit

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,11 @@
+<footer class="footer footer-static footer-dark navbar-border navbar-shadow">
+    <p class="clearfix blue-grey lighten-2 text-sm-center mb-0 px-2">
+        <span class="float-md-left d-block d-md-inline-block">Copyright  &copy; {{Date('Y')}}
+            <a class="text-bold-800 grey darken-2" href="https://github.com/mohit847" target="_blank">Mohit kumar
+            </a>
+        </span>
+        <span class="float-md-right d-block d-md-inline-blockd-none d-lg-block">Made with
+            <i class="ft-heart pink"></i>
+        </span>
+    </p>
+</footer>
